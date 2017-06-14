0f8aa0721a11f2d250ca75108f7278f93fe5379e
Alejandro Alcalde <algui91@gmail.com>
2017-06-14 22:24:48 +0200

    Changing example

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,7 +1,10 @@
 package example
 
-import info.mukel.telegrambot4s._, api._, methods._, models._, Implicits._
 import scala.io.Source
+import scala.util.Random
+
+import info.mukel.telegrambot4s.Implicits._
+import info.mukel.telegrambot4s.api._
 
 object SafeBot extends TelegramBot with Polling with Commands {
   // Use 'def' or 'lazy val' for the token, using a plain 'val' may/will
@@ -11,13 +14,23 @@ object SafeBot extends TelegramBot with Polling with Commands {
     .envOrNone("BOT_TOKEN")
     .getOrElse(Source.fromFile("bot.token").getLines().mkString)
 
-  on("/hello") { implicit msg => _ => reply("My token is SAFE!") }
+  val rng = new Random(System.currentTimeMillis())
+  on("/coin", "head or tail") { implicit msg => _ => reply(if (rng.nextBoolean()) "Head!" else "Tail!") }
+  on("/real", "real number in [0, 1]") { implicit msg => _ => reply(rng.nextDouble().toString) }
+  on("/die", "classic die [1 .. 6]") { implicit msg => _ => reply((rng.nextInt(6) + 1).toString) }
+  on("/dice", "throw two classic dice [1 .. 6]") { implicit msg => _ => reply((rng.nextInt(6) + 1) + " " + (rng.nextInt(6) + 1)) }
+  on("/random", "integer in [0, n)") { implicit msg => {
+    case Seq(Extractor.Int(n)) if n > 0 =>
+      reply(rng.nextInt(n).toString)
+    case _ =>
+      reply("Invalid argumentヽ(ಠ_ಠ)ノ")
+  }
+  }
+  on("/choose", "randomly picks one of the arguments") { implicit msg => args =>
+    reply(if (args.isEmpty) "Empty list." else args(rng.nextInt(args.size)))
+  }
 }
 
-object Hello extends Greeting with App {
-  SafeBot.run()
-}
-
-trait Greeting {
-  lazy val greeting: String = "hello"
+object Hello extends App {
+ // SafeBot.run()
 }
